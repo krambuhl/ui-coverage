11d10bb469c44222b46e904eb91db49b8a8892eb
Evan Krambuhl <thekrambuhl@gmail.com>
2014-10-08 23:37:01 -0700

    nav update

--- source/app/app.js
+++ source/app/app.js
@@ -8,12 +8,19 @@ Coverage.Application = Patchbay.View.extend({
   
   ui: {
     nav: '.l-nav',
+    navControl: '.l-nav-control',
     content: '.l-content'
   },
 
   setup: function() {
     this.nav = Coverage.Navigation.create({ el: this.ui.nav }, { app: this });
     this.content = Coverage.Content.create({ el: this.ui.content }, { app: this });
+
+    this.listenTo(this.ui.navControl, 'click', _.bind(this.toggleNav, this));
+  },
+
+  toggleNav: function() {
+    this.state('nav-expanded', !this.state('nav-expanded'));
   }
 });
 
